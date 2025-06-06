e5895b19df2dd0ed163fc70b7e065b9538b6e8c7
AxelGI <axelvelezg@gmail.com>
2025-06-05 21:15:19 -0600

    set gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/extra-envase-embalaje-2025/', // muy importante
   plugins: [react()],
 })
